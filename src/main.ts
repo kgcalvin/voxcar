f90d001e51290d4e5736c640a49768019de2194a
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2025-05-01 00:35:48 +0300

    run on port 3000

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = 3000;
   await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
