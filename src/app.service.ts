cf1b11b75a8efacf3fbe61e893776071c9c5baa7
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2025-05-01 00:29:06 +0300

    revert back

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is to test that my changes auto deploy!';
+    return 'Hello World!';
   }
 }
