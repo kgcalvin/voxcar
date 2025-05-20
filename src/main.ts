18f89f22f5d784d41e4373aeb394b19d0a064082
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2025-05-20 14:44:42 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Configure CORS
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: '*',
+    credentials: true,
+  });
   const port = 3000;
   await app.listen(port, '0.0.0.0');
 }
