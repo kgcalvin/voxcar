3f5259e167c6f8e2a22dbced34a53096ecfb87cc
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2025-06-04 20:45:06 +0300

    fix domain issue

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Configure CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://voxcar-7b14f.web.app/'],
+    origin: ['http://localhost:3000', 'https://voxcar-7b14f.web.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: '*',
     credentials: true,
