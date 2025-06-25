81206cac126159b0a2cffcf9ece3606124941953
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2025-06-25 16:04:10 +0300

    add domains

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
       'http://localhost:3000',
       'https://voxcar-7b14f.web.app',
       'https://voxcar-fe-ska6.vercel.app',
+      'https://voxcar-fe-b25k.vercel.app',
+      'https://voxcar-fe-gamma.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: '*',
